73ba1e12c19258a679e215f38565612902ff5879
Karabo <karaborabothata0@gmail.com>
2022-07-11 23:43:19 +0200

    update

--- app.js
+++ app.js
@@ -1,73 +1,38 @@
-// import data from './modules/data.mjson'
-const statsContainer = document.getElementById('statsContainer')
+import data from "./modules/data.json" assert { type: "json" };
 
-const data = [
-    {
-      "day": "mon",
-      "amount": 17.45
-    },
-    {
-      "day": "tue",
-      "amount": 34.91
-    },
-    {
-      "day": "wed",
-      "amount": 52.36
-    },
-    {
-      "day": "thu",
-      "amount": 31.07
-    },
-    {
-      "day": "fri",
-      "amount": 23.39
-    },
-    {
-      "day": "sat",
-      "amount": 43.28
-    },
-    {
-      "day": "sun",
-      "amount": 25.48
-    }
-  ]
+const statsContainer = document.getElementById("statsContainer");
 
-function processData(data){
-    let boxes = []
-    for (let i = 0; i < data.length; i++) {
-        const boxContainer = document.createElement('div')
-        const box = document.createElement('div')
-        const pTag = document.createElement('p')
+function processData(data) {
+	let boxes = [];
+	for (let i = 0; i < data.length; i++) {
+		const boxContainer = document.createElement("div");
+		const box = document.createElement("div");
+		const boxPrice = document.createElement("div");
+		const pTag = document.createElement("p");
 
-        boxContainer.className = 'boxContainer'
+		box.className = "main-box";
+		boxContainer.className = "boxContainer";
 
-        box.style.height = `${ 200 / data[i].amount}rem`
-        box.style.margin = '10px'
-        box.className = 'main-box'
-        
+		box.style.height = `${200 / data[i].amount}rem`;
+		box.style.margin = "10px";
 
-        pTag.append(data[i].day)
-        boxContainer.append(box)
-        boxContainer.append(pTag)
+		boxPrice.append(data[i].amount);
 
+		pTag.append(data[i].day);
+		boxContainer.append(boxPrice);
+		boxContainer.append(box);
+		boxContainer.append(pTag);
 
-
-        boxes.push(boxContainer)
-    }
-    // console.log(boxes);
-    return boxes;
+		boxes.push(boxContainer);
+	}
+	return boxes;
 }
 
-function appendData(data){
-    for (let i = 0; i < data.length; i++) {
-        statsContainer.appendChild(data[i])
-    }
+function appendData(data) {
+	for (let i = 0; i < data.length; i++) {
+		statsContainer.appendChild(data[i]);
+		console.log(data[i]);
+	}
 }
 
-appendData(processData(data))
-
-
-// console.log(data[0].amount);
-// processData(data)
-// console.log((processData(data)));
-// statsContainer.appendChild(processData(data))
+appendData(processData(data));
