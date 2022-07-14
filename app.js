73654cb2ada8f1b8799df74dd4b9a5343ca4bc98
Karabo <karaborabothata0@gmail.com>
2022-07-15 00:20:56 +0200

    update

--- app.js
+++ app.js
@@ -1,38 +1,44 @@
 import data from "./modules/data.json" assert { type: "json" };
-
 const statsContainer = document.getElementById("statsContainer");
 
-function processData(data) {
-	let boxes = [];
-	for (let i = 0; i < data.length; i++) {
+// Start processing data from json file
+appendData(processData());
+
+function processData() {
+	let dataStore = [];
+	data.forEach((element) => {
+		// create needed elements
 		const boxContainer = document.createElement("div");
+		const boxAmount = document.createElement("div");
 		const box = document.createElement("div");
-		const boxPrice = document.createElement("div");
-		const pTag = document.createElement("p");
+		const boxDay = document.createElement("p");
 
+		// add class names and dynamic styling
 		box.className = "main-box";
-		boxContainer.className = "boxContainer";
+		boxAmount.className = "boxPrice";
+		box.style.margin = "7px";
+		// value of amounts provides height difference 
+		box.style.height = `${(16 * element.amount) / 100}rem`;
 
-		box.style.height = `${200 / data[i].amount}rem`;
-		box.style.margin = "10px";
 
-		boxPrice.append(data[i].amount);
+		// add eventListners to change class names
+		box.onmouseover = () => (boxAmount.className = "boxPrice-active");
+		box.onmouseleave = () => (boxAmount.className = "boxPrice");
 
-		pTag.append(data[i].day);
-		boxContainer.append(boxPrice);
+		// append to current parent div boxContainer
+		boxDay.append(element.day);
+		boxAmount.append(`$${element.amount}`);
+		boxContainer.append(boxAmount);
 		boxContainer.append(box);
-		boxContainer.append(pTag);
+		boxContainer.append(boxDay);
 
-		boxes.push(boxContainer);
-	}
-	return boxes;
+		// push to array
+		dataStore.push(boxContainer);
+	});
+	return dataStore;
 }
 
-function appendData(data) {
-	for (let i = 0; i < data.length; i++) {
-		statsContainer.appendChild(data[i]);
-		console.log(data[i]);
-	}
+function appendData(dataStore) {
+	// map returned array in param to parent div in HTML
+	dataStore.map((item) => statsContainer.append(item));
 }
-
-appendData(processData(data));
